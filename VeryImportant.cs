e6ff2242467b65c9fe7b2cef87d6515a255b53dc
Daniel <Daniel@DESKTOP>
2022-01-20 12:33:16 -0500

    Bug free check added

---
+++ VeryImportant.cs
@@ -0,0 +1,15 @@
+namespace TeleprompterConsole;
+
+public class VeryImportant
+{
+
+    public void BugFree()
+    {
+        var bugFree = true;
+
+        if(bugFree){
+            Console.WriteLine($"Bug free program: {bugFree}");
+        }
+    }
+
+}
